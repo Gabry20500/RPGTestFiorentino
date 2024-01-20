01f6d7dae4ff177df1a4bc2663c2eda6a3f74ee2
GabrieleFiorentino <60826993+Gabry20500@users.noreply.github.com>
2024-01-20 18:22:31 +0100

    remove files

--- Source/RPGTest/RPGTest.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "RPGTest.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RPGTest, "RPGTest" );
